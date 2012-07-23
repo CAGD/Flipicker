9fbfddd1c34c8a00eeaf025215b5478142d85d67
Graham Hibbert <graham@eyefood.co.uk>
2012-07-23 13:12:59 +0100

    Added bare plugin code. Will put existing code in it later.

--- flipicker.js
+++ flipicker.js
@@ -103,3 +103,48 @@
 				
 		el.closest('.flipicker_wrapper').find('input').val(d.getTime()).change() ;
 	}
+	
+(function( $ ){
+
+  var methods = {
+     init : function( options ) {
+
+       return this.each(function(){
+         $(window).bind('resize.tooltip', methods.reposition);
+       });
+
+     },
+     destroy : function( ) {
+
+       return this.each(function(){
+         $(window).unbind('.tooltip');
+       })
+
+     },
+     reposition : function( ) { 
+       // ... 
+     },
+     show : function( ) { 
+       // ... 
+     },
+     hide : function( ) {
+       // ... 
+     },
+     update : function( content ) { 
+       // ...
+     }
+  };
+
+  $.fn.tooltip = function( method ) {
+    
+    if ( methods[method] ) {
+      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+    }    
+  
+  };
+
+})( jQuery );
